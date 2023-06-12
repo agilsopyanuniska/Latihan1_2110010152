0dec00869c5647baa1cad2f46b6780ee29719eeb
agilsopyanuniska <agilsopyan54@gmail.com>
2023-06-12 17:13:52 +0700

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,5 +2,12 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika agil = new Matematika(5,2);
+        
+        System.out.println("Hasil Penjumlahan : " + agil.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + agil.setPengurangan());
+        System.out.println("Hasil Perkalian : " + agil.setPerkalian());
+        System.out.println("Hasil Pembagian : " + agil.setPembagian());
+    }
 }
